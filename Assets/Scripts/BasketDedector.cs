2c03a45539f1d92c85f501bd2ea1ef6ecb976b2b
Firat Kocabacak <firat0667@hotmail.com>
2024-09-23 13:10:13 +0200

    a

---
+++ Assets/Scripts/BasketDedector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketDedector : MonoBehaviour
+{
+    public delegate void BasketScored();
+    public static event BasketScored OnBasketScored;
+    private bool _istop;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Basketball"))  
+        {
+            if (other.transform.position.y > transform.position.y)
+            {
+                if (OnBasketScored != null)
+                {
+                    _istop = true;
+                }
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.position.y < transform.position.y)
+        {
+            if (_istop)
+            {
+                 OnBasketScored();
+                _istop = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Basketball.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basketball : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public Transform spawnPoint;
+    public float minThrowForce = 5f;
+    public float maxThrowForce = 20f;
+    public Slider powerSlider;
+
+    private float throwForce;
+    private bool isCharging;
+    private float chargeTime = 0f;
+    private float maxChargeTime = 2f;
+
+    public Text scoreText;
+    private int score = 0;
+
+    private void OnEnable()
+    {
+        BasketDedector.OnBasketScored += OnBasketScored;  
+    }
+
+    private void OnDisable()
+    {
+        BasketDedector.OnBasketScored -= OnBasketScored;  
+    }
+
+    void Update()
+    {
+        HandleInput();
+        AdjustPowerSlider();
+    }
+
+    void HandleInput()
+    {
+        float mouseY = Input.mousePosition.y / Screen.height;
+        float angle = Mathf.Lerp(30, 80, mouseY); 
+        spawnPoint.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isCharging = true;
+            chargeTime = 0f;
+            powerSlider.gameObject.SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            chargeTime += Time.deltaTime;
+            float chargePercent = Mathf.Clamp01(chargeTime / maxChargeTime);
+            throwForce = Mathf.Lerp(minThrowForce, maxThrowForce, chargePercent);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isCharging = false;
+            SpawnBall();
+            powerSlider.gameObject.SetActive(false);
+        }
+    }
+
+    void AdjustPowerSlider()
+    {
+        if (isCharging)
+        {
+            float chargePercent = Mathf.Clamp01(chargeTime / maxChargeTime);
+            powerSlider.value = chargePercent;
+        }
+    }
+
+    void SpawnBall()
+    {
+        GameObject ball = Instantiate(ballPrefab, spawnPoint.position, spawnPoint.rotation);
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+        rb.AddForce(-spawnPoint.right * throwForce, ForceMode.Impulse);
+    }
+
+    public void OnBasketScored()
+    {
+        score += 1;
+        scoreText.text = score.ToString();
+    }
+}
